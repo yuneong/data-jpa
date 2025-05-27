1d3dd5aac251ed5d882c7b75a0e2fff9ed2b4208
oyy <oyy0821@chunjae.co.kr>
2025-05-27 13:31:20 +0900

    #1 ~ #6. initial commit

---
+++ src/test/java/study/data_jpa/repository/MemberRepositoryTest.java
@@ -0,0 +1,35 @@
+package study.data_jpa.repository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import study.data_jpa.entity.Member;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@Rollback(false)
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    public void testMember() {
+        Member member = new Member("memberA");
+        Member savedMember = memberRepository.save(member);
+
+//        Optional<Member> findMember = memberRepository.findById(savedMember.getId());
+        Member findMember = memberRepository.findById(savedMember.getId()).get();
+
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember).isEqualTo(member);
+    }
+}
