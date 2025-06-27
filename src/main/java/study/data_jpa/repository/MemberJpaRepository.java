a146cd3e19b50ba4839efba015b81ed9f74953d6
oyy <oyy0821@chunjae.co.kr>
2025-06-27 16:00:00 +0900

    #18. 순수 JPA 페이징과 정렬

--- src/main/java/study/data_jpa/repository/MemberJpaRepository.java
+++ src/main/java/study/data_jpa/repository/MemberJpaRepository.java
@@ -56,4 +56,17 @@ public class MemberJpaRepository {
                 .getResultList();
     }
 
+    public List<Member> findByPage(int age, int offset, int limit) {
+        return em.createQuery("select m from Member m where m.age = :age order by m.username desc", Member.class)
+                .setParameter("age", age)
+                .setFirstResult(offset) // 몇 번째 부터?
+                .setMaxResults(limit) // 몇 개?
+                .getResultList();
+    }
+
+    public long totalCount(int age) {
+        return em.createQuery("select count(m) from Member m where m.age = :age", Long.class)
+                .setParameter("age", age)
+                .getSingleResult();
+    }
 }
